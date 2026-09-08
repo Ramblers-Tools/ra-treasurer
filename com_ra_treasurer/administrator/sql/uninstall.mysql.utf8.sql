fe0560183256ffec0391c551322229507f6a3239
Charlie Bigley <42494431+CharlieBigley@users.noreply.github.com>
2026-09-08 09:35:59 +0100

    Clean up Treasurer system language and obsolete SQL files

--- com_ra_treasurer/administrator/sql/uninstall.mysql.utf8.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS `#__ra_bookings`;
-
-DELETE FROM `#__action_log_config` WHERE (type_alias LIKE 'com_ra_treasurer.%');
-DELETE FROM `#__action_logs_extensions` WHERE (extension = 'com_ra_treasurer');
